d346e043060f4d6612709b6fd6542c833126bdfd
Xie Yuheng <xyheme@gmail.com>
2025-10-10 20:22:34 +0800

    up

--- src/ppml/nodeHelper.ts
+++ src/ppml/nodeHelper.ts
@@ -1,12 +1,56 @@
-import * as PPML from "./index.ts"
+import * as pp from "./index.ts"
 
-export function concatNode(nodes: Array<PPML.Node>): PPML.Node {
+export function group(...nodes: Array<pp.Node>): pp.Node {
+  return pp.GroupNode(concat(...nodes))
+}
+
+export function nil(): pp.Node {
+  return pp.NullNode()
+}
+
+export function br(): pp.Node {
+  return pp.BreakNode(" ")
+}
+
+export function mapWithBreak<A>(
+  render: (x: A) => pp.Node,
+  list: Array<A>,
+): pp.Node {
+  if (list.length === 0) {
+    return pp.nil()
+  } else if (list.length === 1) {
+    return render(list[0])
+  } else {
+    return pp.concat(
+      render(list[0]),
+      pp.br(),
+      mapWithBreak(render, list.slice(1)),
+    )
+  }
+}
+
+export function text(content: string): pp.Node {
+  return pp.TextNode(content)
+}
+
+export function indent(indentation: number, ...nodes: Array<pp.Node>): pp.Node {
+  return pp.IndentNode(indentation, concat(...nodes))
+}
+
+export function indentGroup(
+  indentation: number,
+  ...nodes: Array<pp.Node>
+): pp.Node {
+  return pp.IndentNode(indentation, group(...nodes))
+}
+
+export function concat(...nodes: Array<pp.Node>): pp.Node {
   if (nodes.length === 0) {
-    return PPML.NullNode()
+    return pp.NullNode()
   } else if (nodes.length === 1) {
     return nodes[0]
   } else {
     const [node, ...rest] = nodes
-    return PPML.AppendNode(node, concatNode(rest))
+    return pp.AppendNode(node, concat(...rest))
   }
 }
